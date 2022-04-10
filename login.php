3e2cf7ba203d2d519e9282f09e4851adbf6b10de
JamSaber <jamelracmanjr@gmail.com>
2022-04-11 04:03:30 +0800

    first commit

---
+++ login.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('storeclass.php');
+$store->login();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="form-container">
+            <form action="" method="post">
+
+                <div class="form-meter">
+                    <label for="">Meter Id </label>
+                    <input type="text" name="username" id="MeterId">
+                </div>
+
+                <div class="form-username">
+                    <label for="">Username</label>
+                    <input type="text" name="username" id="username">
+                </div>
+
+                <div class="form-password">
+                    <label for="">Password</label>
+                    <input type="password" name="password" id="password">
+                </div>
+
+                <button type="submit" name="submit">Login</button>
+
+            </form>
+        </div>
+
+    </div>
+</body>
+
+</html>
---
+++ storeclass.php
@@ -0,0 +1,50 @@
+<?php
+
+class MyStore
+{
+    private $server = "mysql:host=localhost;dbname=store";
+    private $user = "root";
+    private $pass = "";
+    private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+    protected $con;
+
+    public function openConnection()
+    {
+        try {
+
+            $this->con = new PDO($this->server, $this->user, $this->pass, $this->options);
+            return $this->con;
+        } catch (PDOException $e) {
+            echo "Have Problem in Connection :" . $e->getMessage();
+        }
+    }
+
+    public function closeConnection()
+    {
+        $this->con = null;
+    }
+
+    public function getUsers()
+    {
+        $connection = $this->openConnection();
+        $stmt = $connection->prepare("SELECT * FROM members");
+        $stmt->execute();
+        $users = $stmt->fetchAll();
+        $userCount = $stmt->rowCount();
+
+        if ($userCount > 0) {
+            return $users;
+        } else {
+            return 0;
+        }
+    }
+
+    public function login()
+    {
+        if (isset($_POST['submit'])) {
+            print_r($_POST);
+        }
+    }
+}
+
+$store = new MyStore();
